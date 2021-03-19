8efac9df593e439e6600068ee94dd8477f095f48
一期一祈 <72604387+wuya-wang@users.noreply.github.com>
2021-03-20 00:45:39 +0800

    增加分类与标签表

---
+++ vueblog/src/components/Article.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="article">
+    <div class="article__title">
+<!--      <h4 v-text="article_title"></h4>-->
+    </div>
+    <div class="article__text" v-html="article_text"></div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Article",
+  data(){
+    return {
+      'article_text':'',
+      'article_title':'',
+    }
+  },
+  created() {
+    this.getArticle()
+  },
+  methods:{
+    getArticle(){
+      axios({
+        url: 'http://127.0.0.1:9999/api/article/',
+        method: 'get',
+      }).then((res) => {
+        console.log(res.data)
+        this.article_title = res.data[0].article_title
+        this.article_text = res.data[0].article_text
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.article__text >>> img {
+  width: -moz-available;
+  margin: 5px 0;
+}
+</style>
---
+++ vueblog/src/components/ArticleList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="article-list">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div>
+          <p class="card-text" style="float: left">点赞</p>
+          <div style="float: right">
+            <i  class="iconfont icon-dianzan"></i>
+            <i class="iconfont icon-shoucang"></i>
+            <i  class="iconfont icon-dashang"></i>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ArticleList",
+  data(){
+    return {
+      article_list: '',
+    }
+  },
+  created() {
+
+  },
+  props:{
+    getAllArticle,
+  },
+  methods:{
+    getArticleList() {
+      axios({
+        url: 'http://127.0.0.1:9999/api/article-list/',
+        method: 'get',
+      }).then((res) => {
+        console.log(res.data)
+      })
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
